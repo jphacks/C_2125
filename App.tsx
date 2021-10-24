b13b21bbc2e454e4e40745e5dfa1e588e6d57d13
YutaUra <yuuta3594@outlook.jp>
2021-10-24 23:11:14 +0900

    style: 💄 カラースキーマを削除

--- App.tsx
+++ App.tsx
@@ -2,19 +2,17 @@ import { StatusBar } from 'expo-status-bar'
 import { NativeBaseProvider } from 'native-base'
 import React from 'react'
 import useCachedResources from './src/hooks/useCachedResources'
-import useColorScheme from './src/hooks/useColorScheme'
 import { Navigation } from './src/navigation'
 
 const App = () => {
   const isLoadingComplete = useCachedResources()
-  const colorScheme = useColorScheme()
 
   if (!isLoadingComplete) {
     return null
   } else {
     return (
       <NativeBaseProvider>
-        <Navigation colorScheme={colorScheme} />
+        <Navigation />
 
         <StatusBar />
       </NativeBaseProvider>
